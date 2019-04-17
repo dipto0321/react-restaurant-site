1556ff8d3acc62ea8edd24cce5d6299edb919e31
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-17 13:23:58 +0600

    Add contact in link

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  BrowserRouter,
-  Link,
-  NavLink,
-  Route,
-  Switch,
+  BrowserRouter, Link, NavLink, Route, Switch,
 } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 
@@ -25,21 +21,23 @@ const About = () => (
   </div>
 );
 
+const Contact = () => (
+  <div className="contact">
+    <h1> This is contact!</h1>
+  </div>
+);
+
 const Header = () => (
   <header>
     <h1>Restaurant Page</h1>
-    <NavLink
-      to="/"
-      activeClassName="active-link"
-      exact
-    >
-        Home
+    <NavLink to="/" activeClassName="active-link" exact>
+      Home
     </NavLink>
-    <NavLink
-      activeClassName="active-link"
-      to="/about"
-    >
-        About
+    <NavLink activeClassName="active-link" to="/about">
+      About
+    </NavLink>
+    <NavLink activeClassName="active-link" to="/contact">
+      Contact
     </NavLink>
   </header>
 );
@@ -48,9 +46,7 @@ const NotFound = () => (
   <div>
     <h1>404 Error</h1>
     <h3>
-      <Link to="/">
-        Home
-      </Link>
+      <Link to="/">Home</Link>
     </h3>
   </div>
 );
@@ -62,6 +58,7 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
         <Route component={NotFound} />
       </Switch>
     </div>
