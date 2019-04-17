e6abd8adda9d4c095ce169afdef16ae9411e9c16
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-17 09:16:02 +0600

    Fix extract-text-plugin related issue
    - Solved by running `yarn add extract-text-webpack-plugin@next -D`

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,10 @@
-import { homeContentGenerator } from '../../home';
-import { aboutContentGenerator } from '../../about';
-import { contactContentGenerator } from '../../contact';
+import { homeContentGenerator } from './home';
+import { aboutContentGenerator } from './about';
+import { contactContentGenerator } from './contact';
+
+require('../scss/main.scss');
+
+require.context('../image', true, /\.(png|svg|jpg|gif)$/);
 
 function generateMainHeader() {
   const header = document.createElement('h1');
