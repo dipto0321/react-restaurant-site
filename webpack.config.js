0b4a77cfaf78811e6a3e844d8f680af79d875c74
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-17 08:59:11 +0600

    Add configuration in webpack.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,36 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/assets/js/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'assets/js/[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: './assets/img',
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
@@ -14,4 +39,11 @@ module.exports = {
     historyApiFallback: true,
   },
   devtool: 'cheap-module-eval-source-map',
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new ExtractTextPlugin('assets/css/main.css'),
+  ],
 };
