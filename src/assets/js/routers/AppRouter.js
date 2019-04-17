01a403b0003b1971cc134de2aae43defb2ece802
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-17 15:28:53 +0800

    Transfer AppRouter into the js/router directory

---
+++ src/assets/js/routers/AppRouter.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Link,
+  NavLink,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h1>About Page</h1>
+  </div>
+);
+
+const Contact = () => (
+  <div className="contact">
+    <h1> This is contact!</h1>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Restaurant Page</h1>
+    <NavLink to="/" activeClassName="active-link" exact>
+      Home
+    </NavLink>
+    <NavLink activeClassName="active-link" to="/about">
+      About
+    </NavLink>
+    <NavLink activeClassName="active-link" to="/contact">
+      Contact
+    </NavLink>
+  </header>
+);
+
+const NotFound = () => (
+  <div>
+    <h1>404 Error</h1>
+    <h3>
+      <Link to="/">Home</Link>
+    </h3>
+  </div>
+);
+
+export default () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
