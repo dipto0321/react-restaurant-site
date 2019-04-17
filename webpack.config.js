4750092f86332ec6bf6b18c902f1c84d01b6a445
Dipto Karmakar <diptokmk47@gmail.com>
2019-04-17 11:25:36 +0600

    Add react in the index and create a template

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,12 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
+      },
     ],
   },
   devServer: {
